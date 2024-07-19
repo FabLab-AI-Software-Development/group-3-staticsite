fc6253c85a3c753d29297226e9eccaa29cb9214d
Michelle Hollis <revenuestarglobal@gmail.com>
2024-07-19 14:37:55 -0600

    about-us

---
+++ src/pages/about-us.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const About = (Props) => {
+    return  (
+        <div>
+            <h1>Hello! We are the Champions!</h1>
+        </div>
+    )
+}
+
+export default About 
