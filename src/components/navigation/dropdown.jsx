e92da3b5a5c26cd258674b145949234e86514319
Miguel Amaya <miguel.e.amaya@gmail.com>
2024-07-19 14:38:31 -0600

    Navigation with react dom

---
+++ src/components/navigation/dropdown.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+const Dropdown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div className="dropdown">
+            <button className="dropdown-toggle" onClick={toggleDropdown}>
+                Toggle Dropdown
+            </button>
+            {isOpen && (
+                <ul className="dropdown-menu">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
+                </ul>
+            )}
+        </div>
+    );
+
+};
+
+export default Dropdown;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import App from './App';
+import Contact from './pages/contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
