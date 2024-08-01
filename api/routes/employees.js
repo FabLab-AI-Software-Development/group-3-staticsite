cdfa84a22024a16a101ea2dc1322eb65e1b27d93
Miguel Amaya <miguel.e.amaya@gmail.com>
2024-08-01 17:30:41 -0600

    Employee controller and router

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {createEmployee}  = require("../controllers/employees")
+const {createEmployee, deleteEmployee, getEmployees, updateEmployee, getEmployee}  = require("../controllers/employees")
 
 router.post("/hire",createEmployee)
+router.get("/",getEmployees)
+router.get("/:id",getEmployee)
+router.delete("/:id",deleteEmployee)
+router.post("/:id",updateEmployee)
 
 module.exports = router
